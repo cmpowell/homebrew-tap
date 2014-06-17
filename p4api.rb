c85f15b323bc73271b3d1b669b823e9779b616fe
Robert Shaw <robert914@me.com>
2014-06-17 12:46:58 -0700

    updated: fixed to be keg only, adds needed Version

--- p4api.rb
+++ p4api.rb
@@ -6,9 +6,18 @@ class P4api < Formula
   version "2014.1.821990"
   sha1 "fdb4db866eb9121e38001c834c091d34216a4d68"
 
+  keg_only <<-EOS
+Perforce C/C++ API package for build only use.
+
+For P4Python supply:
+    --apidir #{HOMEBREW_PREFIX}/opt/#{name.downcase}
+
+EOS
+
   def install
+    prefix.install 'sample/Version'
     lib.install Dir['lib/*']
     (include + 'p4').install Dir['include/p4/*']
-    doc.install Dir['sample/*']
+    doc.install Dir['sample/*.cc', 'sample/Jam*']
   end
 end
