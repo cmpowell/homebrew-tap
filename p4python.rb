0cd7992293d77b8241d1db4fe0ed87f5b1061f5b
Catherine Powell <catherine@abakas.com>
2015-04-30 11:03:19 -0400

    Update SHA for new version of p4python

--- p4python.rb
+++ p4python.rb
@@ -4,7 +4,7 @@ class P4python < Formula
   homepage "http://www.perforce.com/product/components/apis"
   url "http://filehost.perforce.com/perforce/r14.1/bin.tools/p4python.tgz"
   version "2014.1.831633"
-  sha1 "a02f700c9a766dd772d26503186b7afd3238280f"
+  sha1 "d9d3467b4ab36c97f1ae210f4ea76eb08a4510ce"
 
   depends_on 'p4api' => :build
   depends_on :python => :optional
